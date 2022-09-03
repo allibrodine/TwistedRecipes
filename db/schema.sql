7219463bad677991c3163b0386c5dac77e577295
allibrdoine <allibrodine81@gmail.com>
2022-09-03 16:26:49 -0500

    database seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,10 @@ DROP DATABASE IF EXISTS twisted_recipes;
 CREATE DATABASE twisted_recipes;
 
 USE twisted_recipes;
+
+CREATE TABLE classics(
+    id INTEGER NOT NULL,
+    recipe_name VARCHAR(30) NOT NULL,
+    ingredients VARCHAR(500) NOT NULL,
+    method VARCHAR(500) NOT NULL
+);
