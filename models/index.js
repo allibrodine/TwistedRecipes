648555371236a5172ab27d582c47619e95d4508c
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-09-05 10:18:38 -0500

    Updated associations and import models in index.js file under models folder

--- models/index.js
+++ models/index.js
@@ -1,22 +1,23 @@
 //import models
 const Classics = require('./Classics');
 const Comment = require('./Comment');
+const User = require('./User');
 
 //create associations
-// User.hasMany(Comment, {
-//     foreignKey: 'user_id'
-// });
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
 
-// Recipe.hasMany(Comment, {
-//     foreignKey: 'recipe_id'
-// });
+Recipe.hasMany(Comment, {
+    foreignKey: 'recipe_id'
+});
 
-// Comment.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// Comment.belongsTo(Recipe, {
-//     foreignKey: 'recipe_id'
-// });
+Comment.belongsTo(Recipe, {
+    foreignKey: 'recipe_id'
+});
 
-module.exports = { Classics, Comment };
+module.exports = { Classics, Comment, User };
